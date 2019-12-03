f77e679422ae52110a4c2fe31ac6b39030ed5143
mikins1991 <52010686+mikins1991@users.noreply.github.com>
2019-12-03 16:40:20 +0300

    03/12

--- js/script-is.js
+++ js/script-is.js
@@ -39,8 +39,8 @@ jsButton.onclick = () => {
 }
 // task2
 const jsButton1 = document.getElementById('js-button1');
-let jsInput11 = document.getElementById('js-input11');
-let jsInput12 = document.getElementById('js-input12');
+const jsInput11 = document.getElementById('js-input11');
+const jsInput12 = document.getElementById('js-input12');
 
 let jsOut1 = document.getElementById('js-out1');
 
@@ -51,3 +51,61 @@ jsButton1.onclick = () => {
         jsOut1.innerHTML = `${jsInput12.value}`;
     }
 }
+
+// task 40
+
+const jsButton4 = document.getElementById('js-button4');
+const jsInput4 = document.getElementById('js-input4');
+const jsOut4 = document.getElementById('js-out4');
+const jsOut5 = document.getElementById('js-out5');
+const thisYear = 2019;
+
+jsButton4.onclick = () => {
+    let birth = +jsInput4.value;
+    console.log(birth)
+    const age = thisYear - birth;
+    console.log(age);
+    if (age >= 18) {
+        jsOut4.innerHTML = 1;
+    } else {
+        jsOut4.innerHTML = 0;
+    }
+
+    if (birth > 0) {
+        jsOut5.innerHTML = 1;
+    } else if (birth < 0) {
+        jsOut5.innerHTML = 'm';
+    } else {
+        jsOut5.innerHTML = 0;
+    }
+}
+
+// task 8
+
+const jsButton7 = document.getElementById('js-button7');
+const jsInput7 = document.getElementById('js-input7');
+const jsOut7 = document.getElementById('js-out7');
+
+jsButton7.onclick = () => {
+    let balance = jsInput7.value % 2;
+    if (balance == 0) {
+        jsOut7.innerHTML = 'четное';
+    } else {
+        jsOut7.innerHTML = 'нечетное';
+    }
+}
+
+// task 10
+
+const jsButton8 = document.getElementById('js-button7');
+const jsName = document.getElementById('js-name');
+
+jsButton8.onclick = () => {
+    let name;
+    if (jsName.value == '' || jsName.value == ' ') {
+        alert('введите имя');
+    } else {
+        alert('Hello ' + jsName.value);
+    }
+}
+
