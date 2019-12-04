0ed3d0577de94a26081f707b5fb7bef1aeb38364
mikins1991 <52010686+mikins1991@users.noreply.github.com>
2019-12-04 23:42:32 +0300

    Update skript-4.js

--- js/skript-4.js
+++ js/skript-4.js
@@ -20,3 +20,25 @@ document.querySelector('#js-btn').onclick = () => {
         console.log('Не нажат');
     }
 }
+document.getElementById('js-btn2').onclick = (event) => {
+    event.preventDefault();
+    // let textArea = document.querySelector('#js-area');
+    // console.log(textArea.value);
+    let form = document.querySelector('form');
+    console.log(form);
+    console.log(form.elements.area.value);
+    console.log(form.elements.js_input.value);
+}
+
+// task1
+document.querySelector('#js_btn').onclick = () => {
+    alert('Task 1');
+}
+//task2
+document.querySelector('#js_input2').onclick = () => {
+    alert(document.querySelector('#js_input2').value);
+}
+//task3
+document.querySelector('#js_p').onclick = () => {
+    alert('task3');
+}
